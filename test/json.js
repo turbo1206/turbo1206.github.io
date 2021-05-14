5bee5af61054cf88ae974bedbb27a0ee847d2cc6
turbo1206.github.io <turbo4342@gmail.com>
2021-05-14 15:22:17 +0900

    6교시 중간 커밋

---
+++ test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e1260100bd8a46c1b4a29",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Sheppard Webster"
+    },
+    {
+      "_id": "609e126028b7ad95b992229a",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Lamb Boyd"
+    },
+    {
+      "_id": "609e12601056a5fd4633ab0e",
+      "age": 38,
+      "eyeColor": "brown",
+      "name": "Glass Duke"
+    },
+    {
+      "_id": "609e12608fae4ca592c65211",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Fleming Garrison"
+    },
+    {
+      "_id": "609e126012705f7a824fad6c",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Calderon Lambert"
+    },
+    {
+      "_id": "609e12605d8a8e71b5628c0e",
+      "age": 26,
+      "eyeColor": "green",
+      "name": "Rios Park"
+    },
+    {
+      "_id": "609e12602341bcf50187555c",
+      "age": 38,
+      "eyeColor": "green",
+      "name": "Celia Fletcher"
+    },
+    {
+      "_id": "609e1260c090941b211b33b9",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Milagros Randall"
+    },
+    {
+      "_id": "609e126080eeec6f7e905b3d",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Evelyn Mercado"
+    },
+    {
+      "_id": "609e1260761779fc8da8f582",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Ray Rojas"
+    },
+    {
+      "_id": "609e1260413fbe4852cc27d6",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Reed Crosby"
+    },
+    {
+      "_id": "609e12607ab81a1cfe85a63f",
+      "age": 24,
+      "eyeColor": "green",
+      "name": "Christi Donovan"
+    },
+    {
+      "_id": "609e1260f1f9437a73c67db4",
+      "age": 26,
+      "eyeColor": "brown",
+      "name": "Albert Woodward"
+    },
+    {
+      "_id": "609e12605f6913f17d386cc6",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Elvira Harris"
+    },
+    {
+      "_id": "609e126060783b3a39848f0a",
+      "age": 24,
+      "eyeColor": "blue",
+      "name": "Mckay Garner"
+    },
+    {
+      "_id": "609e1260db44134956ba1c9c",
+      "age": 30,
+      "eyeColor": "blue",
+      "name": "Howard Lane"
+    },
+    {
+      "_id": "609e1260d4d1d56a767bd9e1",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Owens Wiggins"
+    },
+    {
+      "_id": "609e12601b4a32cd6951dbfd",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Marina Rocha"
+    },
+    {
+      "_id": "609e126012e904453e663700",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Conner Alvarez"
+    },
+    {
+      "_id": "609e126040cdbffc0ff07dbb",
+      "age": 34,
+      "eyeColor": "brown",
+      "name": "Winifred Barber"
+    }
+  ]
